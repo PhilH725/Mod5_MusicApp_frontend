99c25cc60cb85d0a82903584fe346bae5f7d4db8
github username <github email address>
2019-04-05 20:54:56 -0400

    added some styling for my_albums, still wip, but good for now

--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -1,18 +1,33 @@
 
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {Component, Fragment} from 'react'
+import {Accordion, Icon} from 'semantic-ui-react'
 
-const AlbumItem = (props) => {
+// import {Link} from 'react-router-dom'
+// <Link to={`/items/albums/${this.props.album.id}`}>{this.props.album.name}</Link>
+class AlbumItem extends Component {
+  state = { activeIndex: 0 }
 
-  return (
-    <div>
-      <h4>
-        <Link to={`/items/albums/${props.album.id}`}>
-        {props.album.name}
-        </Link>
-      </h4>
-    </div>
-  )
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Accordion.Title active={this.state.activeIndex === -1} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          {this.props.album.name}
+        </Accordion.Title>
+        <Accordion.Content active={this.state.activeIndex === -1}>
+          <p>song info here</p>
+        </Accordion.Content>
+      </Fragment>
+    )
+  }
 }
 
 export default AlbumItem
--- src/containers/MyAlbumsContainer.js
+++ src/containers/MyAlbumsContainer.js
@@ -1,16 +1,17 @@
 
 import React, {Component} from 'react'
 import AlbumItem from '../components/AlbumItem'
+import {Accordion} from 'semantic-ui-react'
 import {connect} from 'react-redux'
 
 class MyAlbumsContainer extends Component {
 
   render() {
     return (
-      <div>
+      <Accordion>
         <h3>My Albums:</h3>
         {this.props.albums.map(a => <AlbumItem key={a.id} album={a} />)}
-      </div>
+      </Accordion>
     )
   }
 }
