1e15a333206851f68c07e119f55738552708dd14
github username <github email address>
2019-04-08 22:34:02 -0400

    made song info modal open on click of div rather than button

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,24 +1,31 @@
 
-import React from 'react'
+import React, {Component} from 'react'
 import SongItemModal from './SongItemModal'
-import {Item, Modal, Button} from 'semantic-ui-react'
+import {Item, Modal} from 'semantic-ui-react'
+// trigger={<Button size="tiny" compact>More Info</Button>}
+class SongItem extends Component {
+  state = {
+    open: false
+  }
 
-const SongItem = (props) => {
+  onClose = () => this.setState({open: false});
+  openModal = () => this.setState({open: true})
 
-  return (
-    <Item>
-      <Item.Image size='mini' src={require('../img/image.png')} />
+  render() {
+    return (
+      <Item onClick={this.openModal}>
+        <Item.Image size='mini' src={require('../img/image.png')} />
 
-      <Item.Content>
-        <Item.Header>{props.song.name}</Item.Header>
-        <Item.Meta>{props.song.artist} - {props.song.album}</Item.Meta>
-      </Item.Content>
-      <Modal trigger={<Button size="tiny" compact>More Info</Button>} centered={false}>
-        <SongItemModal song={props.song} />
-      </Modal>
-    </Item>
-
-  )
+        <Item.Content>
+          <Item.Header>{this.props.song.name}</Item.Header>
+          <Item.Meta>{this.props.song.artist} - {this.props.song.album}</Item.Meta>
+        </Item.Content>
+        <Modal open={this.state.open} onClose={this.onClose} centered={false}>
+          <SongItemModal song={this.props.song} />
+        </Modal>
+      </Item>
+    )
+  }
 }
 
 export default SongItem
