95621b7489c2e7ed2c9f64c07587ce0473279059
github username <github email address>
2019-04-16 09:05:17 -0400

    styled create playlist

--- src/components/NewPlaylistSongItem.js
+++ src/components/NewPlaylistSongItem.js
@@ -6,14 +6,16 @@ const NewPlaylistSongItem = (props) => {
 
   return (
     <Item>
-    <Button size="mini" float="right" onClick={()=>props.handleClick(props.song)} icon>
-      <Icon name={props.buttonText}/>
-    </Button>
       <Image size='mini' src={props.song.album.image} alt={'album-art'} />
 
-      <Item.Content>
+      <Item.Content id="create-playlist-text">
         <Item.Header>{props.song.name.slice(0,60)}</Item.Header>
         <Item.Meta>{props.song.artist} - {props.song.album.name}</Item.Meta>
+      </Item.Content>
+      <Item.Content id="create-playlist-button-container" float="right">
+        <Button inverted color="linkedin" size="mini" float="right" onClick={()=>props.handleClick(props.song)} icon>
+          <Icon name={props.buttonText}/>
+        </Button>
       </Item.Content>
     </Item>
 
