caa850a22d9dd6480f571af3577ddc7e69bbf0b8
github username <github email address>
2019-04-16 22:04:25 -0400

    added a logo and styled the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 
 import React from 'react'
+import {Image} from 'semantic-ui-react'
 
+// <h1 className="ui header" id="site-header">SpotiPhil</h1>
 const Header = () => {
 
   return (
-    <div>
-      <h1 className="ui header" id="site-header">SpotiPhil</h1>
+    <div className="ui header" id="site-header">
+      <Image id="official-logo" src={require('../img/spotiphil5.png')}/>
+      <div id="site-name">SpotiPhil</div>
     </div>
   )
 }
