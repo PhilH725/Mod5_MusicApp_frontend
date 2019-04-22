4c6a10f915b7ed789c04625518ed59731c17062c
github username <github email address>
2019-04-22 18:31:47 -0400

    laid out edit playlist songs table

---
+++ src/components/EditPlaylistSongRow.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import {Table, Button, Icon} from 'semantic-ui-react'
+
+const EditPlaylistSongRow = (props) => {
+
+  return (
+    <Table.Row >
+      <Table.HeaderCell textAlign='center'>{props.trackNumber}</Table.HeaderCell>
+      <Table.HeaderCell>{props.song.name}</Table.HeaderCell>
+      <Table.HeaderCell>{props.song.artist}</Table.HeaderCell>
+      <Table.HeaderCell>{props.song.album}</Table.HeaderCell>
+      <Table.HeaderCell>
+        <Button icon>
+          <Icon name="arrow down" />
+        </Button>
+        <Button icon>
+          <Icon name="arrow up" />
+        </Button>
+      </Table.HeaderCell>
+    </Table.Row>
+  )
+}
+
+export default EditPlaylistSongRow
--- src/containers/EditPlaylist.js
+++ src/containers/EditPlaylist.js
@@ -1,7 +1,8 @@
 
 import React, { Component } from 'react'
 import EditPlaylistNameForm from '../components/EditPlaylistNameForm'
-import {Header, Form} from 'semantic-ui-react'
+import EditPlaylistSongRow from '../components/EditPlaylistSongRow'
+import {Header, Divider, Table} from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import {fetchingPlaylistToEdit} from '../redux/actionCreators'
 
@@ -18,6 +19,21 @@ class EditPlaylist extends Component {
         <Header id="my-music-header" as="h2" dividing>Editing Playlist</Header>
         <Header id="subheader-notice">{this.props.editingPlaylist.name}</Header>
         <EditPlaylistNameForm playlist={this.props.editingPlaylist} />
+        <Divider />
+        <Table celled selectable striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell width={1} textAlign='center'>#</Table.HeaderCell>
+              <Table.HeaderCell width={6}>Song Name</Table.HeaderCell>
+              <Table.HeaderCell width={4}>Artist</Table.HeaderCell>
+              <Table.HeaderCell width={3}>Album</Table.HeaderCell>
+              <Table.HeaderCell width={2}>Actions</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.editingPlaylist.songs.map((s, index) => <EditPlaylistSongRow key={index} song={s} trackNumber={index+1}/>)}
+          </Table.Body>
+        </Table>
       </div>
       :
       <h3>loading...</h3>
