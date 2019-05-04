47ed4c21984519645084fdfd935ee7e615ffd465
github username <github email address>
2019-05-04 18:55:13 -0400

    reverted song modal to using local state

--- src/components/SongItemModal.js
+++ src/components/SongItemModal.js
@@ -5,8 +5,28 @@ import {getVideoId, toggleMusicPlayback} from '../redux/actionCreators'
 import {connect} from 'react-redux'
 
 class SongItemModal extends Component {
+  constructor() {
+    super()
+    this.state = {
+      videoId: ""
+    }
+  }
 
+  //im currently using local state and redux to save the video id. local state looks better for the current modal display
+  //because it shows loading instead of the previous video, but redux will eventually be used to have the video play
+  //in a different component. when i make a final decision to use one ill delete the other.
   componentDidMount() {
+    fetch('http://localhost:3000/get_song_url', {
+      method: 'POST',
+      headers: {"Content-Type":"application/json", Accept:"application/json"},
+      body: JSON.stringify({
+        songName: this.props.song.name,
+        artistName: this.props.song.artist
+      })
+    })
+    .then(res => res.json())
+    .then(id => this.setState({videoId: id.id}))
+
     this.props.getVideoId(this.props.song.name, this.props.song.artist)
     this.props.toggleMusicPlayback()
   }
@@ -18,9 +38,9 @@ class SongItemModal extends Component {
           <Header id="youtube-modal-name-header" as="h2">{this.props.song.name}</Header>
           <Header id="youtube-modal-info-header" as="h3">{this.props.song.artist} - {this.props.song.album.name}</Header>
           <Divider />
-          {this.props.videoId.length > 0 ?
+          {this.state.videoId.length > 0 ?
           <div id="youtube-video-container">
-            <iframe id="youtube-video-player" title="song" src={`https://www.youtube.com/embed/${this.props.videoId}`}>
+            <iframe id="youtube-video-player" title="song" src={`https://www.youtube.com/embed/${this.state.videoId}`}>
             </iframe>
           </div>
           :
@@ -30,8 +50,6 @@ class SongItemModal extends Component {
     )
   }
 }
-// src="https://www.youtube.com/embed/emGri7i8Y2Y"
-// https://www.youtube.com/watch?v=emGri7i8Y2Y
 
 const mapStateToProps = state => ({
   videoId: state.videoId
